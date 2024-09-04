3a8d83a5894acebf9b49bffae5585f4e93325a3d
MarcosEduadoPereira <162046549+MarcosEduadoPereira@users.noreply.github.com>
2024-09-03 22:51:38 -0300

    Exercícios

---
+++ EstagioTargetRibeiraoPreto/src/estagio01/Exercicio05.java
@@ -0,0 +1,21 @@
+package estagio01;
+
+public class Exercicio05 {
+	
+	/* 5-) Identificar qual interruptor controla qual lâmpada 
+
+		Ligue o primeiro interruptor e deixe-o ligado por alguns minutos.
+		
+		Desligue o primeiro interruptor e ligue o segundo interruptor.
+		
+		Vá até as salas das lâmpadas:
+		
+		A lâmpada que está acesa é controlada pelo segundo interruptor (o que você deixou ligado recentemente).
+		
+		Toque nas outras lâmpadas. A lâmpada que estiver quente (mas apagada) é controlada pelo primeiro interruptor (o que você deixou ligado por um tempo).
+		
+		A lâmpada fria e apagada é controlada pelo terceiro interruptor (o que você nunca ligou). */
+	
+	
+	
+}
